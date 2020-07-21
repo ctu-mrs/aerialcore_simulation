3713ff68bee4ef270fd65d8ee459780fb4338074
Your Name <you@example.com>
2020-07-21 16:20:12 +0200

    Finished DynamicMap and Powerline plugins.

--- src/world_plugins/powerline_gazebo_plugin.cc
+++ src/world_plugins/powerline_gazebo_plugin.cc
@@ -22,16 +22,18 @@ namespace gazebo
 
     private:
       const unsigned int TILE_SIZE = 256;    
-      const int _spawn_distance = 4;
-      std::vector<ignition::math::Vector2d> _loaded_pylon_positions;
-      std::vector<ignition::math::Vector2d> _spawned_pylon_positions;
+      const int spawn_distance = 500;
+      std::set<ignition::math::Vector3d> loaded_pylon_positions;
+      std::set<ignition::math::Vector3d> spawned_pylon_positions;
       ignition::math::Vector2d center;
       int _prev_pylon_index = 0;
+      int refresh_rate = 5;
+      int prev_second = 0;
       physics::WorldPtr _world = NULL;
       ignition::math::Vector3d uav_position_;
       // Gazebo subscribers
       std::string file_path = "/home/mrs/workspace/src/aerialcore_simulation/gps/v223-v224_gps.txt";      
-      
+      std::string model_name = "somepowerline"; 
       transport::NodePtr       gz_node_;
       transport::SubscriberPtr gz_uav_sub_;
       transport::NodePtr gz_node_pub_;
@@ -39,12 +41,14 @@ namespace gazebo
       transport::PublisherPtr factory_pub_;
       // parameters
       bool exists = false;
-      std::string _uav_name_;
+      std::string uav_name;
       // callbacks
       void CallbackGazeboUavPose(const GazeboPosesStampedConstPtr& msg);
 
-      void SpawnModel(std::string modelName, double lat, double lon, double rotation_rad);
+      void SpawnModel(ignition::math::Vector3d position);
+      void DespawnModel(ignition::math::Vector3d position);
 
+      void CheckDistances(ignition::math::Vector3d uavPosition);
       std::string getFileName(const std::string& s);
 
   };
@@ -60,14 +64,18 @@ namespace gazebo
     this->_world = _world;
     // get model that will be tracked
     if (!_sdf->HasElement("uav_name")) {
-      _uav_name_ = "uav1";
-      ROS_INFO("Use default reference frame: %s",_uav_name_.c_str());
+      uav_name = "uav1";
+      ROS_INFO("Use default reference frame: %s",uav_name.c_str());
     }else{
-      _uav_name_ = _sdf->Get<std::string>("uav_name");
-      ROS_INFO("Use set reference frame: %s",_uav_name_.c_str());
+      uav_name = _sdf->Get<std::string>("uav_name");
+      ROS_INFO("Use set reference frame: %s",uav_name.c_str());
     }
     if (_sdf->HasElement("file_path"))
       file_path = _sdf->Get<std::string>("file_path");
+    if (_sdf->HasElement("model_name"))
+      model_name = _sdf->Get<std::string>("model_name");
+    if (_sdf->HasElement("refresh_rate"))
+      refresh_rate = _sdf->Get<int>("refresh_rate");
 
   if (!_sdf->HasElement("center"))
   {
@@ -105,6 +113,9 @@ namespace gazebo
     std::array<double, 2> WGS84Position;
     //std::string file_name = this->getFileName(file_path);
     std::string file_name = _sdf->GetName();
+    
+    std::vector<ignition::math::Vector3d> pylonsFromFile;
+
     while(std::getline(csv_file, line))
     {
       std::stringstream line_stream(line);
@@ -114,29 +125,119 @@ namespace gazebo
       line_stream >> lon;
       WGS84Position = {lat,lon};
       std::array<double, 2> result{wgs84::toCartesian(WGS84Reference, WGS84Position)};
-
-      SpawnModel(file_name, result[0], result[1], 0);
+      pylonsFromFile.push_back(ignition::math::Vector3d(result[0],result[1],0));
     }
     csv_file.close();
-    
-  }          
+    int totalSize = pylonsFromFile.size();
+ gzerr << "Loaded " << totalSize << "positions"; 
+    ignition::math::Vector3d previousPylon;
+    ignition::math::Vector3d currentPylon = pylonsFromFile.back();
+    pylonsFromFile.pop_back();
+    ignition::math::Vector3d nextPylon = pylonsFromFile.back();
+    pylonsFromFile.pop_back();
+ 
+    double rotation1 = std::atan2(nextPylon.Y()-currentPylon.Y(), nextPylon.X()-currentPylon.X());
+    double rotation2;
+    currentPylon.Z(rotation1);
+    loaded_pylon_positions.insert(currentPylon);
+
+      for (int i = 0; i <= totalSize-3; i++) {
+
+        previousPylon = currentPylon;
+        currentPylon = nextPylon;
+        nextPylon = pylonsFromFile.back();
+        pylonsFromFile.pop_back();
+        rotation1 = std::atan2(nextPylon.Y()-currentPylon.Y(), nextPylon.X()-currentPylon.X());
+        rotation2 = std::atan2(currentPylon.Y()-previousPylon.Y(), currentPylon.X()-previousPylon.X());
+        currentPylon.Z((rotation1+rotation2)/2);
+        loaded_pylon_positions.insert(currentPylon);
+      }
+      previousPylon = currentPylon;
+      currentPylon = nextPylon;
+      rotation2 = std::atan2(currentPylon.Y()-previousPylon.Y(), currentPylon.X()-previousPylon.X());
+      currentPylon.Z(rotation2);
+      loaded_pylon_positions.insert(currentPylon);
+ gzerr << "Loaded all positions"; 
+
+  }
+
+  void PowerlinePlugin::CheckDistances(ignition::math::Vector3d uavPosition) {
+    std::vector<ignition::math::Vector3d> addedPylons;   
+    std::vector<ignition::math::Vector3d> removedPylons;
+    double distance;
+    ignition::math::Vector3d currentPylon;
+
+  for (auto it = loaded_pylon_positions.begin(); it != loaded_pylon_positions.end(); ) {
+    distance = std::sqrt(std::pow((uavPosition.X()-it->X()),2)+std::pow(uavPosition.X()-it->Y(),2));
+    if(distance <= spawn_distance) {
+    std::cout << "Spawning"<<std::endl;
+      addedPylons.push_back(*it);
+      this->SpawnModel(*it);
+      it = loaded_pylon_positions.erase(it);
+    }else{
+      ++it;
+      }
+    }
+
+  for (auto it = spawned_pylon_positions.begin(); it != spawned_pylon_positions.end(); ) {
+    distance = std::sqrt(std::pow((uavPosition.X()-it->X()),2)+std::pow(uavPosition.X()-it->Y(),2));
+    if(distance > spawn_distance) {
+    std::cout << "Despawning"<<std::endl;
+      removedPylons.push_back(*it);
+      this->DespawnModel(*it);
+      it = spawned_pylon_positions.erase(it);
+    }else{
+      ++it;
+      }
+    }
+    for (auto pylon : addedPylons){
+      spawned_pylon_positions.insert(pylon); 
+    }
+    for (auto pylon : removedPylons){
+      loaded_pylon_positions.insert(pylon); 
+    }
+  }
 
   void PowerlinePlugin::CallbackGazeboUavPose(const GazeboPosesStampedConstPtr& msg) {
     // here comes the spawning and despawning logic
-    return;
+    
+    int time = msg->time().sec(); 
+    //std::cout << time << " " << time % refresh_rate << std::endl; 
+    if (prev_second == time || time % refresh_rate != 0) {
+     return; 
+    }
+    prev_second = time;
+
+      bool exists = false;
+      ignition::math::Vector3d uav_position_;
+      for (int i = 0; i < msg->pose_size(); i++) {
+        if (msg->pose(i).name() == this->uav_name) {
+  /* std::cout << "found uav"<< std::endl; */
+    exists = true;
+          uav_position_.X() = msg->pose(i).position().x();
+          uav_position_.Y() = msg->pose(i).position().y();
+          uav_position_.Z() = msg->pose(i).position().z(); 
+          break;
+        }
+      }
+
+    if (exists)
+      this->CheckDistances(uav_position_);
+
+
   }
 
-  void PowerlinePlugin::SpawnModel(std::string model_name, double x, double y, double rotation_rad)
+  void PowerlinePlugin::SpawnModel(ignition::math::Vector3d position)
   {
     
       sdf::SDF newSDF;
       std::stringstream newStream;
 
       newStream << "<sdf version ='1.5'>\n" <<
-         "   <model name='"<< model_name <<"_"<<x<<"_"<<y<<"'>\n" <<
+         "   <model name='"<< this->model_name <<"_"<<position[0]<<"_"<<position[1]<<"'>\n" <<
          "     <static>true</static>\n" <<
          "     <link name='link'>\n" <<
-         "       <pose>"<<x<<" "<<y<<" 0 0 0 "<<rotation_rad<<"</pose>\n" <<
+         "       <pose>"<<position[0]<<" "<<position[1]<<" 0 0 0 "<<position[2]<<"</pose>\n" <<
          "       <visual name='visual'>\n" <<
          "         <cast_shadows>false</cast_shadows>\n" <<
          "         <geometry>\n" <<
@@ -161,6 +262,14 @@ namespace gazebo
       factory_pub_->Publish(msgfac);
   }
 
+      void PowerlinePlugin::DespawnModel(ignition::math::Vector3d position) {
+      std::stringstream newStream; 
+        newStream << this->model_name <<"_"<<position[0]<<"_"<<position[1];
+msgs::Request *msg = gazebo::msgs::CreateRequest("entity_delete", newStream.str());
+      this->gz_msg_pub_->Publish(*msg,true);
+      delete msg;
+
+      }
   std::string getFileName(const std::string& s) {
 
      char sep = '/';
